c4476c5fc2538970d56db83f1e7d3a21c0c14145
seungyoung Yun <soungyoung14@gmail.com>
2026-07-01 18:11:52 +0900

    260701 18시 프로그래머스 기초 3일차 완료

--- workspace/Programmers/level01/day03/Prob12.java
+++ workspace/Programmers/level01/day03/Prob12.java
@@ -2,7 +2,7 @@ package level01.day03;
 
 public class Prob12 {
     public static void main(String[] args) {
-        // 프로그래머스 기초 Day3 - 11 문자리스트를 문자열로 반환
+        // 프로그래머스 기초 Day3 - 12 문자리스트를 문자열로 반환
         // 배열 생성과 동시에 선언
         String[] arr = {"a", "b", "c"};
         String answer = "";
