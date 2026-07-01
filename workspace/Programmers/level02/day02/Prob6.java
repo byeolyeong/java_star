c7ef49f8447af6952ba83a852578d6c08299fe77
seungyoung Yun <soungyoung14@gmail.com>
2026-07-01 18:54:05 +0900

    260701 18:50 level2 day2 Prob6 코드 오류 확인 후 수정 완료

--- workspace/Programmers/level02/day02/Prob6.java
+++ workspace/Programmers/level02/day02/Prob6.java
@@ -2,7 +2,7 @@ package level02.day02;
 
 public class Prob6 {
     public int solution(int num1, int num2) {
-        int answer = ((num1 >= num2) ? -1 : 1);
+        int answer = num1 == num2 ? 1 : -1;
         return answer;
     }
 
