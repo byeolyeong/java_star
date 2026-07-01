a04019345601be39cf9643d227558d86b116a6f7
seungyoung Yun <soungyoung14@gmail.com>
2026-07-01 16:38:12 +0900

    260701 회고 ex14-01.html 예제 작성 완료. + 함수, DOM, 이벤트, 노드 찾기 예제 실습 완료

---
+++ workspace/05_javascript/login/login.js
@@ -0,0 +1,12 @@
+const loginForm = document.querySelector('.login-form');
+// loginForm에 submit가 발생함
+loginForm.addEventListener('submit', function(event){
+  console.log('로그인 요청');
+  
+  // 브라우저의 유효성 검증 결과를 확인(required, pattern등)
+  if(!loginForm.checkValidity()){
+    event.preventDefault(); // 브라우저의 기본 동작 취소(submit 취소)
+    return;
+  }
+
+});
